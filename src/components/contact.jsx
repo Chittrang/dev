9e0f1ff93916208aaef27d34ba226209b314565f
Chittrang <chittrang.m@gmail.com>
2025-04-18 23:02:55 +0530

    media for mobile and images centered

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,7 +39,7 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfyjFrXtREeqeDwLufCI_Nsg0Npkxra0quY5PcSHz-RuiAm6w/viewform?embedded=true" width="640" height="1171" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfyjFrXtREeqeDwLufCI_Nsg0Npkxra0quY5PcSHz-RuiAm6w/viewform?embedded=true" width="640" height="1171" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
         </div>
       </div>
       <div id="footer">
